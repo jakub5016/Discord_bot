2a913e16d7d63aab2acdf9ee50f524cf071e689d
Jakub Gil <263455@student.pwr.edu.pl>
2022-04-16 20:27:13 +0200

    First version that can be use without many bugs

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # bot.py
 import argparse
+from glob import glob
 import os
 from socket import timeout
 from termios import CLNEXT
@@ -19,6 +20,9 @@ import requests
 
 queue = []
 queue_time = []
+queue_titles = []
+wantToSkip = False
+
 
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
 
@@ -63,6 +67,7 @@ async def leave(ctx):
     await ctx.send('Bye bye :wave:')
     queue.clear()
     queue_time.clear()
+    queue_titles.clear()
 
 @bot.command(name='add', help='Adding song to queue')
 async def add(ctx, msg):
@@ -83,21 +88,60 @@ async def add(ctx, msg):
 
             queue.append(audio.url)
             queue_time.append(song.length)
+            queue_titles.append(song.title)
 
             await ctx.send("Adding to queue:")
             await ctx.send(song.title)
+            if not (ctx.voice_client.is_playing()):
+                await play(ctx=ctx)
 
-@bot.command(name='play', help='Adding song to queue')
+
 async def play(ctx):
     server = ctx.message.guild
     voice_channel = server.voice_client
-    for i in range(len(queue)):
-        await ctx.send(i)
-        while ctx.voice_client.is_playing():
-            await asyncio.sleep(5)
-        source = discord.FFmpegPCMAudio(source=queue[i], **FFMPEG_OPTIONS)
-        voice_channel.play(source=source, after=voice_channel.stop())
-        time.sleep(2)
-        await ctx.send(i)
 
+    global queue
+    global queue_time
+    global queue_titles
+
+    theteIsMore = True
+    global wantToSkip
+    i = 0 
+    while theteIsMore and (not wantToSkip): #main loop while plaing
+        
+        while (ctx.voice_client.is_playing() and (not wantToSkip)):
+            await asyncio.sleep(1)
+        
+        if i >= len(queue):
+            theteIsMore = False
+        
+        if wantToSkip: #If you want to skip there is exception
+            if i == len(queue): #If you are at the end, just clear queue
+                queue.clear()
+                queue_time.clear()
+                queue_titles.clear()
+            voice_channel.stop() #Stop music and go to next song
+            wantToSkip = False
+        
+        try:
+            source = discord.FFmpegPCMAudio(source=queue[i], **FFMPEG_OPTIONS)
+            voice_channel.play(source=source, after=voice_channel.stop())
+        except:
+            queue.clear()
+            queue_time.clear()
+            queue_titles.clear()
+        finally:
+            i += 1
+            time.sleep(2)
+    
+
+@bot.command(name='que', help='Showing a queue')
+async def queue_check(ctx):
+    for i in range(len(queue_titles)):
+        await ctx.send(str(i+1) + ' ' + queue_titles[i])
+
+@bot.command(name='skip', help='Adding song to queue')
+async def skip_song(ctx):
+    global wantToSkip
+    wantToSkip= True
 bot.run(TOKEN) #join server as bot
